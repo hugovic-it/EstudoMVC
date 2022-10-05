c3777818d896990ef9c9f0e42433a19b6d82b45c
Hugo Victor <hugovic.it@gmail.com>
2022-10-05 15:27:22 -0300

    model

---
+++ Models/Filme.cs
@@ -0,0 +1,12 @@
+namespace EstudoMVC.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+        public string Avaliacao { get; set; }
+    }
+}
